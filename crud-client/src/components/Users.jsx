bfa1c4555abd1c6a0b2d20d9b06cb51036366809
Fariha Promi <farihapromi2611@gmail.com>
2025-05-22 21:53:16 +0600

    Add delete functionailty

--- crud-client/src/components/Users.jsx
+++ crud-client/src/components/Users.jsx
@@ -1,8 +1,34 @@
 import React from 'react'
+import { useLoaderData } from 'react-router-dom'
 
 const Users = () => {
+    const users=useLoaderData()
+    console.log(users)
+    const handleDelete=(_id)=>{
+      
+fetch(`http://localhost:5000/users/${_id}`,{
+    method:'DELETE'
+
+})
+.then(res=>res.json())
+.then(data=>console.log(data))
+.catch(error=>console.log(error))
+
+    }
   return (
     <div>
+        {
+            users.map(user=>(
+                             <div className='flex items-center justify-center' key={user._id}>
+                
+                <h2>{user.name}: {user.email}</h2>
+                <button className='bg-blue-400 p-4 m-4  rounded-lg' onClick={()=>handleDelete(user._id)}>Delete User </button>
+                </div>
+            )
+        
+        )
+    }
+       
       
     </div>
   )
--- simple-first-server/index.js
+++ simple-first-server/index.js
@@ -10,7 +10,7 @@ app.get('/', (req, res) => {
   res.send();
 });
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri =
   'mongodb+srv://farihapromi2611:farihaPromi1998@cluster0.u7yariw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 
@@ -39,6 +39,13 @@ async function run() {
       const users = req.body;
       console.log('users', users);
       const result = await userCollection.insertOne(users);
+      res.send(result);
+    });
+
+    app.delete('/users/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await userCollection.deleteOne(query);
       res.send(result);
     });
     // Send a ping to confirm a successful connection
